a7866db396409f5fe9aacca6ea6d0d71020af745
Gorelih Denis <nppfan96@mail.ru>
2024-08-05 16:20:35 +0500

    homework

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,25 +1,25 @@
 "use strict";
 
-const userForm = new UserForm();
+// const userForm = new UserForm();
 
-userForm.loginFormCallback = function(data) {
-	ApiConnector.login(data, (response) => {
-		if (response.success) {
-			location.reload();
-		} else {
-			console.error(response.error);
-			userForm.setErrorMessage(response.error);
-		}
-	});
-};
+// userForm.loginFormCallback = function(data) {
+// 	ApiConnector.login(data, (response) => {
+// 		if (response.success) {
+// 			location.reload();
+// 		} else {
+// 			console.error(response.error);
+// 			userForm.setErrorMessage(response.error);
+// 		}
+// 	});
+// };
 
-userForm.registerFormCallback = function(data) {
-	ApiConnector.register(data, (response) => {
-		if (response.success) {
-			location.reload();
-		} else {
-			console.error(response.error);
-			userForm.setErrorMessage(response.error);
-		}
-	});
-};
+// userForm.registerFormCallback = function(data) {
+// 	ApiConnector.register(data, (response) => {
+// 		if (response.success) {
+// 			location.reload();
+// 		} else {
+// 			console.error(response.error);
+// 			userForm.setErrorMessage(response.error);
+// 		}
+// 	});
+// };
