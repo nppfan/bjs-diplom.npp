bc1bfad375ebe153cd7133587d94d6979470ec30
Gorelih Denis <nppfan96@mail.ru>
2024-08-05 15:55:52 +0500

    homework

--- public/loginPage.js
+++ public/loginPage.js
@@ -3,14 +3,10 @@
 const userForm = new UserForm();
 
 userForm.loginFormCallback = function(data) {
-
 	ApiConnector.login(data, (response) => {
-
 		if (response.success) {
-
 			location.reload();
 		} else {
-
 			console.error(response.error);
 			userForm.setErrorMessage(response.error);
 		}
@@ -18,14 +14,10 @@ userForm.loginFormCallback = function(data) {
 };
 
 userForm.registerFormCallback = function(data) {
-
 	ApiConnector.register(data, (response) => {
-
 		if (response.success) {
-
 			location.reload();
 		} else {
-
 			console.error(response.error);
 			userForm.setErrorMessage(response.error);
 		}
